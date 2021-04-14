12d2349d75be9a42e485538d29b26b4b12d27cce
Connor Holland <supersoccergoalie123@gmail.com>
2021-04-13 20:06:55 -0600

    Added routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   resources :port_stocks
   resources :stocks
-  resources :portfolios
-  resources :users
+  resources :portfolios do
+    resources :stock, only: [:index]
+  end
+  resources :users do
+    resources :portfolios
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
