10488c30fd51655e50e3f1108f7ae06e3384a276
Connor Holland <supersoccergoalie123@gmail.com>
2021-04-14 10:38:06 -0600

    Functionality to add new portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,24 @@
 class PortfoliosController < ApplicationController
   include UsersHelper
   before_action :require_logged_in
-  
+
   def index
+    @user = current_user
+    @portfolios = current_user.portfolios
+  end
+
+  def create
+    user_id = new_port_params[1].to_i
+    portfolio = Portfolio.new(name: new_port_params[0], user_id: user_id)
+    if portfolio.save
+      redirect_to user_portfolios_path(user_id)
+    else
+
+    end
+  end
+
+  private
+  def new_port_params
+    params.values_at(:name, :user_id)
   end
 end
