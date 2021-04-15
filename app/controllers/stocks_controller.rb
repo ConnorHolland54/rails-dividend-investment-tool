8fc0f203f40bf9f697dc20bf652e8f1b18fdf9e0
Connor Holland <supersoccergoalie123@gmail.com>
2021-04-14 18:08:06 -0600

    Setup styling for the news and table view

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,18 @@
 class StocksController < ApplicationController
   def index
+    @user = user?
+    if @user
+      @user = params[:user_id]
+    else
+      @user = nil
+    end
+  end
+
+  private
+  def user?
+    if params[:user_id]
+      return true
+    end
+    return false
   end
 end
